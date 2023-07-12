d4644ff05f44907ceb0465e43823ef041b3727e4
can7wysiwyg <paulkssa@gmail.com>
2023-07-12 09:52:45 +0200

    cart functionality

---
+++ src/api/CartApi.js
@@ -0,0 +1,76 @@
+export const addItem = (item, next) => {
+    let cart = [];
+    if (typeof window !== "undefined") {
+        if (localStorage.getItem("cart")) {
+            cart = JSON.parse(localStorage.getItem("cart"));
+        }
+        cart.push({
+            ...item,
+            count: 1
+        });
+
+        cart = Array.from(new Set(cart.map(p => p._id))).map(id => {
+            return cart.find(p => p._id === id);
+        });
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+
+        next();
+    }
+};
+
+export const itemTotal = () => {
+    if (typeof window !== "undefined") {
+        if (localStorage.getItem("cart")) {
+            return JSON.parse(localStorage.getItem("cart")).length;
+        }
+    }
+    return 0;
+};
+
+export const getCart = () => {
+    if (typeof window !== "undefined") {
+        if (localStorage.getItem("cart")) {
+            return JSON.parse(localStorage.getItem("cart"));
+        }
+    }
+    return [];
+};
+
+export const updateItem = (bookId, count) => {
+    let cart = [];
+    if (typeof window !== "undefined") {
+        if (localStorage.getItem("cart")) {
+            cart = JSON.parse(localStorage.getItem("cart"));
+        }
+
+        cart.map((book, i) => {
+            if (book._id === bookId) {
+                cart[i].count = count;
+            }
+            return book;
+        });
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+};
+
+export const removeItem = bookId => {
+    let cart = [];
+    if (typeof window !== "undefined") {
+        if (localStorage.getItem("cart")) {
+            cart = JSON.parse(localStorage.getItem("cart"));
+        }
+
+        cart = cart.filter(book => book._id !== bookId);
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+};
+
+export const emptyCart = next => {
+    if (typeof window !== "undefined") {
+        localStorage.removeItem("cart");
+        next();
+    }
+};
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,8 +1,12 @@
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { GlobalState } from "../../GlobalState";
-import './navbar.css'; 
+import './navbar.css';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { itemTotal } from "../../api/CartApi"
+
 
 const NavBar = () => {
   const state = useContext(GlobalState);
@@ -56,24 +60,27 @@ const NavBar = () => {
     }
   };
 
-
-
-
-
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-       {Webname()}
-
+      {Webname()}
       <Navbar.Toggle aria-controls="navbar-nav" />
       <Navbar.Collapse id="navbar-nav">
-      {AdminLogin()}
-        <Nav className="ml-auto">
+        <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/books">Books</Nav.Link>
           <Nav.Link href="/new_books">New Books</Nav.Link>
           <Nav.Link href="/search_books">Search Books</Nav.Link>
           <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav>
+        <Nav className="ml-auto">
           {isLogged ? loggedRouter() : ""}
+          {AdminLogin()}
+
+          <Nav.Link href="/cart" className="d-flex justify-content-center justify-content-lg-start">
+  <FontAwesomeIcon icon={faShoppingCart} />
+  <span>{itemTotal()}</span>
+</Nav.Link>
+          
         </Nav>
       </Navbar.Collapse>
     </Navbar>
