1113e585501ea362b6e0c89af2836696f8e41fda
can7wysiwyg <paulkssa@gmail.com>
2023-07-13 15:04:04 +0200

    changes

--- src/components/admin/CartShow.js
+++ src/components/admin/CartShow.js
@@ -1,9 +1,33 @@
+import { useContext, useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
+import { GlobalState } from "../../GlobalState"
+import axios from "axios"
 
 function CartShow() {
    const location = useLocation()
+   const state = useContext(GlobalState)
+   const token = state.token
+   const[items, setItems] = useState([])
 
-//    location.state.itemName
+   useEffect(() => {
+const getCartItems = async() => {
+
+    const res = await axios.get(`/cartt/show_cus_orders/fn?fullname=${location.state.itemName}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    setItems(res.data.data);
+
+}
+
+getCartItems()
+
+   }, [location.state.itemName, token])
+
+
+   
     
     return(<>
     
